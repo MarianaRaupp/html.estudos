b48c9fa6bbea009e4b8424f5b1f5ce646fcfaf9c
MarianaRaupp <marianaraupp09@gmail.com>
2025-06-04 17:02:38 -0300

    Add files via upload

---
+++ html/parImpar.js
@@ -0,0 +1,17 @@
+alert ("digite ZERO para sair")
+do {
+ num = Number (prompt("digite um numero: "))
+
+ if (isNaN(num)){
+   alert ("valor digitado nao é um numero")
+   continue 
+ } any 
+
+ if (num == 0 || isNaN (num))
+
+ break 
+
+num%2==0?alert ("numero par" + num ):
+alert ("numero impar" + num )
+
+} while (true)
