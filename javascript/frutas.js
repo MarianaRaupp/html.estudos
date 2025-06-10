b6edfe45d329a179706b8f2f0c67e5f15efe946f
MarianaRaupp <marianaraupp09@gmail.com>
2025-06-10 17:05:55 -0300

    Add files via upload

---
+++ javascript/frutas.js
@@ -0,0 +1,11 @@
+const frutas = ["uva", "laranja", "tangerina", "o lucas é estranho", "quero dar pro will", "to com saudade do will", "O LUCAS GOSTA DE DAR A BUNDA"]
+console.log(frutas)
+
+function buscarporposicao() {
+    posicao= prompt ("escolha por um elemento arrey")
+    alert (frutas[posicao])
+
+}
+function tamanhodoarrey(){
+    alert (frutass.length)
+}
